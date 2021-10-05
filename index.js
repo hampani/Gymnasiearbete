405d6dfb03f4068e88a7f9f4bab4c2f3ce116d8a
hampani <hampus.lowing@outlook.com>
2021-10-05 23:07:55 +0200

    implementerade bubble sort

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const range = 100;
+let isSorting = false;
 
 // Elements
 const parentDiv = document.getElementById("sorting-container");
@@ -10,6 +11,8 @@ noOfElementsSlider.oninput = () => {
   generate();
 };
 
+let currentArr = [];
+
 const generateRandomArray = (noOfElements) => {
   // Hard coded range to 100. Varför skulle man vilja ändra range?
   const arr = [];
@@ -18,31 +21,88 @@ const generateRandomArray = (noOfElements) => {
     //Kan teoretiskt sätt vara 0?
     arr.push(Math.floor(Math.random() * range) + 1);
   }
-  return arr;
+  currentArr = arr;
 };
 
 const displayArray = (arr) => {
-  arr.forEach((elemNumber) => {
+  arr.forEach((elemNumber, index) => {
     const element = document.createElement("div");
 
     element.classList.add("element");
     element.style.height = elemNumber + "%";
+    element.id = index;
 
     parentDiv.appendChild(element);
   });
 };
 
 const generate = () => {
+  if (isSorting) return;
   parentDiv.innerHTML = "";
-  displayArray(generateRandomArray(noOfElementsSlider.value));
+  generateRandomArray(noOfElementsSlider.value);
+  displayArray(currentArr);
 };
 
 const handleSortPress = () => {
+  if (isSorting) return;
   radioButtons.forEach((elem) => {
     if (elem.checked) {
-      console.log(elem.value);
+      const delay = document.getElementById("delaySlider").value * 10;
+      if (elem.value === "bubble_sort") {
+        bubbleSort(delay);
+      }
     }
   });
+};
+
+const swap = async (a, b, delay) => {
+  // Byt styles
+  a.style.backgroundColor = "red";
+  b.style.backgroundColor = "red";
+
+  await new Promise((resolve) =>
+    setTimeout(() => {
+      resolve();
+    }, delay)
+  );
+
+  const temp = a.style.transfrom;
+  a.style.transfrom = b.style.transfrom;
+  b.style.transfrom = temp;
+
+  parentDiv.insertBefore(b, a);
+};
+
+const bubbleSort = async (delay) => {
+  isSorting = true;
+  const array = currentArr;
+  const elements = parentDiv.children;
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      elements[j].style.backgroundColor = "blue";
+      elements[j + 1].style.backgroundColor = "blue";
+
+      await new Promise((resolve) =>
+        setTimeout(() => {
+          resolve();
+        }, delay)
+      );
+
+      if (array[j] > array[j + 1]) {
+        await swap(elements[j], elements[j + 1], delay);
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+
+      elements[j].style.backgroundColor = "white";
+      elements[j + 1].style.backgroundColor = "white";
+    }
+
+    elements[elements.length - i - 1].style.backgroundColor = "green";
+  }
+  isSorting = false;
 };
 
 generate();
